2c6222d41978475a4a7ebc0dee846e22126a05fb
安井 利紗 <yasui05821@gmail.com>
2016-07-12 16:30:03 +0900

    日程追加, 日程、申込みリンク等レスポンシブ対応

---
+++ frontend/assets/data/schedule.js
@@ -0,0 +1,61 @@
+module.exports = [
+    {
+        code: "01",
+        date: "9/1"
+    },
+    {
+        code: "02",
+        date: "9/1"
+    },
+    {
+        code: "03",
+        date: "9/1"
+    },
+    {
+        code: "04",
+        date: "9/1"
+    },
+    {
+        code: "05",
+        date: "9/1"
+    },
+    {
+        code: "06",
+        date: "9/1"
+    },
+    {
+        code: "07",
+        date: "9/1"
+    },
+    {
+        code: "08",
+        date: "9/1"
+    },
+    {
+        code: "09",
+        date: "9/1"
+    },
+    {
+        code: "01",
+        date: "9/1"
+    },
+    {
+        code: "01",
+        date: "9/1"
+    },{
+        code: "01",
+        date: "9/1"
+    },
+    {
+        code: "01",
+        date: "9/1"
+    },
+    {
+        code: "01",
+        date: "9/1"
+    },
+    {
+        code: "01",
+        date: "9/1"
+    }
+]
