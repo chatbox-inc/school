3b08471c293b65334888ba939fbda015b1fb706a
安井 利紗 <yasui05821@gmail.com>
2016-07-12 18:27:54 +0900

    #entryBlockへのページ内リンク 位置をjQueryで調整

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -7,10 +7,8 @@ $(function(){
 //btn-entryの調整
 $(function(){
     $('.btn-entry').click(function() {
-        console.log("てすてす");
         var speed = 400;
-        var href= $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
+        var target = $("#entryBlock");
         var headerHeight = 35;
         var position = target.offset().top - headerHeight;
         $('body,html').animate({scrollTop:position}, speed, 'swing');
