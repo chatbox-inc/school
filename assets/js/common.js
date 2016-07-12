68b81dc5ec8c74ca7ad5640fe434435370dc5d1f
安井 利紗 <yasui05821@gmail.com>
2016-07-12 13:52:18 +0900

    title修正、申込みリンク追加( peatex)、OGP画像追加、日程項目追加

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,5 +2,18 @@ $(function(){
     $('#gmap').on('click',function(){
         $('#gmap iframe').css("pointer-events", "auto");
     });
+});
 
-})
+//btn-entryの調整
+$(function(){
+    $('.btn-entry').click(function() {
+        console.log("てすてす");
+        var speed = 400;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var headerHeight = 35;
+        var position = target.offset().top - headerHeight;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+    });
+});
